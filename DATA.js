56fb60ac6ed96036acf1081760302f457a3c9a6b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-06 03:08:43 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "a7de06bb-7833-43b3-a330-0a6506f7529f",
+    "home_feed": [
+        {
+            "id": "2cd91a7f-f072-4244-9fc8-7527815bc345",
+            "artist_name": "behavior",
+            "home_carousel": [
+                {
+                    "id": "c1c36dfb-7774-4bf8-b53c-ade9688b6f3e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                },
+                {
+                    "id": "bf2be1be-d261-461c-9640-7b511bc0f26b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/290.png"
+                },
+                {
+                    "id": "e3d314e7-f324-4a86-84c3-e4db745310a8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                },
+                {
+                    "id": "22515a38-49f7-417a-81d5-0ed3c863235c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "07fc4d30-97e6-477b-aee3-fde654243fde",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/258.png"
+                }
+            ],
+            "art_caption": "true",
+            "art_description": "Performance more near firm. Visit operation and huge black. High enter player take system rest."
+        },
+        {
+            "id": "980f7229-cb8d-4823-88c3-525b3b06cb34",
+            "artist_name": "of",
+            "home_carousel": [
+                {
+                    "id": "de9864c0-7756-45a3-9880-b7c9667abf8e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "44872e75-a4fe-4dea-b504-1ea860a43080",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png"
+                },
+                {
+                    "id": "4d4b47f9-2c25-4617-afc7-28ac77ac2a48",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png"
+                },
+                {
+                    "id": "cbd0fe2d-23cc-4243-9ccc-1336f36afc2e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "33a0f544-9eab-40c1-ad04-1e92edf1f000",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                }
+            ],
+            "art_caption": "simple",
+            "art_description": "Necessary weight point above charge particular wrong. With option character deal become natural."
+        },
+        {
+            "id": "07bcf219-2653-4e3e-a873-b09f6f40e776",
+            "artist_name": "officer",
+            "home_carousel": [
+                {
+                    "id": "2ad56c31-ebf6-44a3-a933-4eb265f8d6e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "ad7b1ecf-f4fd-43a5-8c62-672e4ee57b05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "23fbdcba-a0a4-4953-acaa-e3acf8420ebf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                },
+                {
+                    "id": "d9d311fd-0128-421c-8222-4bad023cc46b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "04b07ac3-252e-4a06-80b4-d3e1e2682182",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                }
+            ],
+            "art_caption": "health",
+            "art_description": "Woman establish reach camera worry. Cell rich front. Write book picture in."
+        },
+        {
+            "id": "965470db-ed53-4759-9c7e-54799dd411fe",
+            "artist_name": "piece",
+            "home_carousel": [
+                {
+                    "id": "302b6212-86ca-4cf7-9c8e-2ecf38e29884",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "be38a8e0-a6c2-4a7d-9e3f-f49b5f07592b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "f0df3ab5-ea42-4d4b-b368-98e2777745c1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                },
+                {
+                    "id": "5f63e4d2-e8d9-42ba-88ed-5c0849aa4094",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "bdb9c74c-686c-48aa-a4e3-7d7e6936736b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                }
+            ],
+            "art_caption": "main",
+            "art_description": "Especially water score amount drop. Send police wife fast personal everyone list."
+        },
+        {
+            "id": "29be3347-aaed-4835-9714-3137431a20e6",
+            "artist_name": "pretty",
+            "home_carousel": [
+                {
+                    "id": "b4835bdc-b413-4c74-b254-e8d4b5d38a72",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "b7038396-2fd8-4255-8845-18f485745802",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "8b618041-b716-4d4f-b95f-ce188b471141",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png"
+                },
+                {
+                    "id": "85a78bfb-5df7-4c89-b209-b71ae5ac46c4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "a6f675aa-1525-41d4-bce9-2b5f212db984",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                }
+            ],
+            "art_caption": "parent",
+            "art_description": "Worry among couple kid dinner around tend special. Participant statement win must call. Employee miss various despite your."
+        }
+    ]
+}
